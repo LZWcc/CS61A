84777230066b9543d6605e2990dd290892340ae4
LZWcc <l1274645232@gmail.com>
2025-05-15 20:52:12 +0800

    add Representation

---
+++ Code_in_class/Representation.py
@@ -0,0 +1,20 @@
+"""
+str对人类可读
+repr对python解释器可读
+>>> 12e12
+12000000000000.0
+>>> print(repr(12e12))
+12000000000000.0
+>>> eval(repr(object)) == object
+True
+>>> eval(repr(object)) is object
+True
+
+>>> from fractions import Fraction
+>>> half = Fraction(1, 2)
+>>> repr(half)
+'Fraction(1, 2)'
+>>> str(half)
+'1/2'
+"""
+
