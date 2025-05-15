ec5aa35f55182d3a24528ba3fc3cb558b39a0797
LZWcc <l1274645232@gmail.com>
2025-05-16 00:20:32 +0800

    update_Representation

--- Code_in_class/Representation.py
+++ Code_in_class/Representation.py
@@ -13,10 +13,44 @@ True
 >>> from fractions import Fraction
 >>> half = Fraction(1, 2)
 >>> repr(half)
+'Fraction(1, 2)'
 >>> print(half)
 1/2
-'Fraction(1, 2)'
 >>> str(half)
 '1/2'
 """
 
+"""
+>>> s = "Hello, world!"
+>>> s
+'Hello, world!'
+>>> print(repr(s))
+'Hello, world!'
+>>> print(s)
+Hello, world!
+>>> print(str(s))
+Hello, world!
+>>> str(s)
+'Hello, world!'
+>>> repr(s)
+"'Hello, world!'"
+"""
+
+"""
+>>> from math import pi
+>>> f'pi start with {pi}...'
+'pi start with 3.141592653589793...'
+"""
+
+# Polymorphic Functions 多态函数
+"""
+>>> half = Fraction(1, 2)
+>>> half.__repr__ ()
+'Fraction(1, 2)'
+>>> half.__str__ ()
+'1/2'
+
+"""
+
+def repr(x):
+    return type(x).__repr__(x)
