62a85b2667038b765647cf3a1347ece840ce24fa
LZWcc <l1274645232@gmail.com>
2025-05-24 21:25:46 +0800

    add:hw07.scm

---
+++ hw/hw07/hw07.scm
@@ -0,0 +1,19 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let (_________________)
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) 'YOUR-CODE-HERE)
+
+(define (caddr s) 'YOUR-CODE-HERE)
+
+(cond ((> x 10)) (print 'big))
+      ((> x 5)) (print 'medium)
+      (else (print 'small))
